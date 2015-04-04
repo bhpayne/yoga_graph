87016ae08ab51e112970ab162ef67b8f307a02ba
Ben Payne <ben.is.located@gmail.com>
2015-04-04 14:06:15 -0400

    minor changes

---
+++ pose_pictures/test.py
@@ -0,0 +1,31 @@
+import os, re, Tkinter, ImageTk
+
+def ls_rec(direc, filter_fun=lambda a:True):
+    for (dirname, dirnames, fnames) in os.walk(direc):
+        for fname in fnames:
+            if filter_fun(fname):
+                yield os.path.join(dirname,fname)
+
+
+top = Tkinter.Tk()
+image_label = Tkinter.Label(top)
+text_label = Tkinter.Label(top,text="Below is an image")
+images = ls_rec(os.getcwd(), lambda a:re.match('.*\\.jpg$',a))
+
+imgL = []
+
+def get_next_image(event = None):
+    fname = images.next()
+    print fname
+    fhandle = open(fname)
+    img = ImageTk.PhotoImage(file=fhandle)
+    fhandle.close()
+    imgL.append(img)
+    image_label.config(image=img)
+
+
+top.bind('<Return>',get_next_image)
+image_label.pack(side='bottom')
+text_label.pack(side='top')
+get_next_image()
+top.mainloop()
--- yoga_graph.py
+++ yoga_graph.py
@@ -19,8 +19,8 @@ DG=nx.DiGraph() # initialize directed graph using networkx
 DG=ydb.pose_properties(DG) # load node properties
 DG=ydb.pose_transitions(DG) # load edges
 
-# ylib.plot_graph_nodes(DG) # generate visual graph using networkx
-# ylib.plot_graph_with_labels(DG,'english_name') # generate visual graph, plot using matplotlib
+#ylib.plot_graph_nodes(DG) # generate visual graph using networkx
+#ylib.plot_graph_with_labels(DG,'english_name') # generate visual graph, plot using matplotlib
 
 [viewer,use_viewer,directory_containing_pictures,entry_point_index,max_poses,delay,field_value]=ylib.get_inputs("config.input")
 
@@ -29,7 +29,7 @@ DG=ydb.pose_transitions(DG) # load edges
 # print(entry_point_index)
 # print(max_poses)
 
-# pose_history=ylib.random_flow(DG,entry_point_index,max_poses,field_value,delay,viewer,use_viewer)
+pose_history=ylib.random_flow(DG,entry_point_index,max_poses,field_value,delay,viewer,use_viewer)
 
-filename="20140627.graphml"
-nx.write_graphml(DG,filename)
+#filename="20140627.graphml"
+#nx.write_graphml(DG,filename)
