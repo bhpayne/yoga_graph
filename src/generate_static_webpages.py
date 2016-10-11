292f72e0ff7961613b5c07596955591559473193
Ben Payne <ben.is.located@gmail.com>
2016-10-11 10:17:07 -0400

    easier browsing

--- src/generate_static_webpages.py
+++ src/generate_static_webpages.py
@@ -39,7 +39,12 @@ for this_indx in range(len(DG)):
 	f.write("<TITLE>yoga graph: "+dic_for_this_node['english_name']+"</TITLE>")
 	f.write("</head>\n<BODY>\n\n<table border=\"1\" width=\"100%\">\n")
 	f.write("<TR width=\"50%\">\n\t<TD>Current pose: <font size=\""+large_font_size+"\">"+dic_for_this_node['english_name']+"</font> </TD>\n")
-	f.write("\t<TD>Adjacent poses</TD>\n</TR>\n<TR>\n")
+	f.write("\t<TD>Adjacent poses: \n")
+	list_of_edges_for_this_node=DG[this_indx].keys()
+	for neighbor_indx in list_of_edges_for_this_node:
+		dic_for_adjacent_node=DG.nodes(data=True)[neighbor_indx][1]
+		f.write("\t\t<a href=\""+str(neighbor_indx)+".html\">"+dic_for_adjacent_node['english_name']+"</a> | \n")		
+	f.write("\t</TD>\n</TR>\n<TR>\n")
 	f.write("\t<TD valign=\"top\">\n")
 	if (dic_for_this_node['yogajournal_picture'] != ""):
 		f.write("\t<a href=\""+dic_for_this_node['yogajournalurl']+"\">\n")
@@ -70,7 +75,8 @@ for this_indx in range(len(DG)):
 
 		
 	f.write("\t</TD>\n</TR>\n</table>\n")
-	f.write("\t<a href=\"http://bhpayne.github.io/yoga_graph/site/\">http://bhpayne.github.io/yoga_graph/site/</a><BR>\n")
+	f.write("\t<P>&nbsp;</P>\n")
+	f.write("\t<center><a href=\"http://bhpayne.github.io/yoga_graph/\">http://bhpayne.github.io/yoga_graph/</a></center><BR>\n")
 	f.write("\t<P>&nbsp;</P>\n")
 	f.write("\t<P>&nbsp;</P>\n")
 	f.write("\t<P>&nbsp;</P>\n")
