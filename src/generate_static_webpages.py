4e05e4011f02bf82d4b96c966e69af79ade7ad4f
Ben Payne <ben.is.located@gmail.com>
2021-06-19 17:28:04 -0400

    webpage link order is consistent

--- src/generate_static_webpages.py
+++ src/generate_static_webpages.py
@@ -32,122 +32,128 @@ write_to_path = "../site/generated_pages/"
 # DG[0]
 
 for this_indx in range(len(DG)):
+
     dic_for_this_node = DG.nodes(data=True)[this_indx]
     print("index: " + str(this_indx) + "; " + dic_for_this_node["english_name"])
-    f = open(write_to_path + str(this_indx) + ".html", "w")
-    f.write("<HTML>\n<HEAD>\n")
-    f.write("<TITLE>yoga graph: " + dic_for_this_node["english_name"] + "</TITLE>")
-    f.write(
-        "</head>\n<BODY>\n\n<table border=\"1\" style='table-layout:fixed;width:100%'>\n"
-    )
-    f.write(
-        '<TR width="50%">\n\t<TD>Current pose: <font size="'
-        + large_font_size
-        + '">'
-        + dic_for_this_node["english_name"]
-        + "</font> </TD>\n"
-    )
-    f.write("\t<TD>Adjacent poses: \n")
-    list_of_edges_for_this_node = DG[this_indx].keys()
-    for neighbor_indx in list_of_edges_for_this_node:
-        dic_for_adjacent_node = DG.nodes(data=True)[neighbor_indx]
+    with open(write_to_path + str(this_indx) + ".html", "w") as f:
+        f.write("<HTML>\n<HEAD>\n")
+        f.write("<TITLE>yoga graph: " + dic_for_this_node["english_name"] + "</TITLE>")
         f.write(
-            '<a href="'
-            + str(neighbor_indx)
-            + '.html">'
-            + dic_for_adjacent_node["english_name"]
-            + "</a> | \n"
+            "</head>\n<BODY>\n\n<table border=\"1\" style='table-layout:fixed;width:100%'>\n"
         )
-    f.write("\n\t</TD>\n</TR>\n<TR>\n")
-    f.write('\t<TD valign="top">\n')
-    if dic_for_this_node["yogajournal_picture"] != "":
-        f.write('\t<a href="' + dic_for_this_node["yogajournalurl"] + '">\n')
         f.write(
-            '\t<img src="'
-            + dic_for_this_node["yogajournal_picture"]
-            + '" width="'
-            + image_width
-            + '"></a><BR>\n'
-        )
-        # f.write("\timage source: <a href=\""+dic_for_this_node['yogajournal_picture']+"\">"+dic_for_this_node['yogajournal_picture']+"</a><BR>\n")
-        f.write(
-            '\t<font size="'
-            + small_font_size
-            + '">source:  <a href="'
-            + dic_for_this_node["yogajournalurl"]
-            + '">Yoga Journal</a></font><BR>\n'
-        )
-    if dic_for_this_node["Dharma Mittra picture URL"] != "":
-        f.write(
-            '\t<BR>\n\t<img src="'
-            + dic_for_this_node["Dharma Mittra picture URL"]
-            + '" width="'
-            + image_width
-            + '"><BR>\n'
-        )
-        # f.write("\timage source: <a href=\""+dic_for_this_node['Dharma Mittra picture URL']+"\">"+dic_for_this_node['Dharma Mittra picture URL']+"</a><BR>\n")
-        f.write(
-            '\t<font size="'
-            + small_font_size
-            + '">source:  <a href="http://www.dharmayogacenter.com/resources/yoga-poses/view-all-yoga-poses-by-category/">dharmayogacenter.com</a></font><BR>\n'
-        )
-    f.write("\n\t</TD>\n\t<TD>\n")
-    list_of_edges_for_this_node = DG[this_indx].keys()
-    for neighbor_indx in list_of_edges_for_this_node:
-        dic_for_adjacent_node = DG.nodes(data=True)[neighbor_indx]
-        f.write("\t<P>\n")
-        f.write(
-            '\t<font size="'
+            '<TR width="50%">\n\t<TD>Current pose: <font size="'
             + large_font_size
-            + '"><a href="'
-            + str(neighbor_indx)
-            + '.html">'
-            + dic_for_adjacent_node["english_name"]
-            + "</a></font><BR>\n"
+            + '">'
+            + dic_for_this_node["english_name"]
+            + "</font> </TD>\n"
         )
-        if dic_for_adjacent_node["yogajournal_picture"] != "":
-            f.write('\t<a href="' + str(neighbor_indx) + '.html">\n')
+        f.write("\t<TD>Adjacent poses: \n")
+
+        list_of_edges_for_this_node = list(DG[this_indx].keys())
+        list_of_edges_for_this_node.sort() # same order each time
+
+        # text list in upper right quadrant
+        for neighbor_indx in list_of_edges_for_this_node:
+            dic_for_adjacent_node = DG.nodes(data=True)[neighbor_indx]
+            f.write(
+                '<a href="'
+                + str(neighbor_indx)
+                + '.html">'
+                + dic_for_adjacent_node["english_name"]
+                + "</a> | \n"
+            )
+        f.write("\n\t</TD>\n</TR>\n<TR>\n")
+        f.write('\t<TD valign="top">\n')
+        if dic_for_this_node["yogajournal_picture"] != "":
+            f.write('\t<a href="' + dic_for_this_node["yogajournalurl"] + '">\n')
             f.write(
                 '\t<img src="'
-                + dic_for_adjacent_node["yogajournal_picture"]
+                + dic_for_this_node["yogajournal_picture"]
                 + '" width="'
                 + image_width
                 + '"></a><BR>\n'
             )
-            # f.write("\timage source: <a href=\""+dic_for_adjacent_node['yogajournal_picture']+"\">"+dic_for_adjacent_node['yogajournal_picture']+"</a><BR>\n")
+            # f.write("\timage source: <a href=\""+dic_for_this_node['yogajournal_picture']+"\">"+dic_for_this_node['yogajournal_picture']+"</a><BR>\n")
             f.write(
                 '\t<font size="'
                 + small_font_size
                 + '">source:  <a href="'
-                + dic_for_adjacent_node["yogajournalurl"]
+                + dic_for_this_node["yogajournalurl"]
                 + '">Yoga Journal</a></font><BR>\n'
             )
-        if dic_for_adjacent_node["Dharma Mittra picture URL"] != "":
-            f.write('\t<a href="' + str(neighbor_indx) + '.html">\n')
+        if dic_for_this_node["Dharma Mittra picture URL"] != "":
             f.write(
                 '\t<BR>\n\t<img src="'
-                + dic_for_adjacent_node["Dharma Mittra picture URL"]
+                + dic_for_this_node["Dharma Mittra picture URL"]
                 + '" width="'
                 + image_width
-                + '"></a><BR>\n'
+                + '"><BR>\n'
             )
-            # f.write("\timage source: <a href=\""+dic_for_adjacent_node['Dharma Mittra picture URL']+"\">"+dic_for_adjacent_node['Dharma Mittra picture URL']+"</a><BR>\n")
+            # f.write("\timage source: <a href=\""+dic_for_this_node['Dharma Mittra picture URL']+"\">"+dic_for_this_node['Dharma Mittra picture URL']+"</a><BR>\n")
             f.write(
                 '\t<font size="'
                 + small_font_size
                 + '">source:  <a href="http://www.dharmayogacenter.com/resources/yoga-poses/view-all-yoga-poses-by-category/">dharmayogacenter.com</a></font><BR>\n'
             )
-        f.write("\t</P>\n\t<HR>\n")
+        f.write("\n\t</TD>\n\t<TD>\n")
 
-    f.write("\t</TD>\n</TR>\n</table>\n")
-    f.write("\t<P>&nbsp;</P>\n")
-    f.write(
-        '\t<center><a href="http://bhpayne.github.io/yoga_graph/">http://bhpayne.github.io/yoga_graph/</a></center><BR>\n'
-    )
-    f.write("\t<P>&nbsp;</P>\n")
-    f.write("\t<P>&nbsp;</P>\n")
-    f.write("\t<P>&nbsp;</P>\n")
-    f.write("<center>Ben Payne<BR>\n")
-    f.write("ben.is.located AT gmail</center>\n")
-    f.write("</BODY>\n</HTML>")
-    f.close()
+        # lower right quadrant
+        for neighbor_indx in list_of_edges_for_this_node:
+            dic_for_adjacent_node = DG.nodes(data=True)[neighbor_indx]
+            f.write("\t<P>\n")
+            f.write(
+                '\t<font size="'
+                + large_font_size
+                + '"><a href="'
+                + str(neighbor_indx)
+                + '.html">'
+                + dic_for_adjacent_node["english_name"]
+                + "</a></font><BR>\n"
+            )
+            if dic_for_adjacent_node["yogajournal_picture"] != "":
+                f.write('\t<a href="' + str(neighbor_indx) + '.html">\n')
+                f.write(
+                    '\t<img src="'
+                    + dic_for_adjacent_node["yogajournal_picture"]
+                    + '" width="'
+                    + image_width
+                    + '"></a><BR>\n'
+                )
+                # f.write("\timage source: <a href=\""+dic_for_adjacent_node['yogajournal_picture']+"\">"+dic_for_adjacent_node['yogajournal_picture']+"</a><BR>\n")
+                f.write(
+                    '\t<font size="'
+                    + small_font_size
+                    + '">source:  <a href="'
+                    + dic_for_adjacent_node["yogajournalurl"]
+                    + '">Yoga Journal</a></font><BR>\n'
+                )
+            if dic_for_adjacent_node["Dharma Mittra picture URL"] != "":
+                f.write('\t<a href="' + str(neighbor_indx) + '.html">\n')
+                f.write(
+                    '\t<BR>\n\t<img src="'
+                    + dic_for_adjacent_node["Dharma Mittra picture URL"]
+                    + '" width="'
+                    + image_width
+                    + '"></a><BR>\n'
+                )
+                # f.write("\timage source: <a href=\""+dic_for_adjacent_node['Dharma Mittra picture URL']+"\">"+dic_for_adjacent_node['Dharma Mittra picture URL']+"</a><BR>\n")
+                f.write(
+                    '\t<font size="'
+                    + small_font_size
+                    + '">source:  <a href="http://www.dharmayogacenter.com/resources/yoga-poses/view-all-yoga-poses-by-category/">dharmayogacenter.com</a></font><BR>\n'
+                )
+            f.write("\t</P>\n\t<HR>\n")
+
+        f.write("\t</TD>\n</TR>\n</table>\n")
+        f.write("\t<P>&nbsp;</P>\n")
+        f.write(
+            '\t<center><a href="http://bhpayne.github.io/yoga_graph/">http://bhpayne.github.io/yoga_graph/</a></center><BR>\n'
+        )
+        f.write("\t<P>&nbsp;</P>\n")
+        f.write("\t<P>&nbsp;</P>\n")
+        f.write("\t<P>&nbsp;</P>\n")
+        f.write("<center>Ben Payne<BR>\n")
+        f.write("ben.is.located AT gmail</center>\n")
+        f.write("</BODY>\n</HTML>")
+        f.close()
