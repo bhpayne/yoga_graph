c9802bf4186f8e801363e5303c4f2f09dc3b737b
Ben Payne <ben.is.located@gmail.com>
2021-06-20 16:40:25 -0400

    every image now has a source

--- src/generate_static_webpages.py
+++ src/generate_static_webpages.py
@@ -11,6 +11,7 @@ This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 Inte
 To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/.
 """
 
+import os
 import glob
 import networkx as nx  # format for directed graph
 import yoga_db as ydb  # nodes and edges of graph
@@ -32,7 +33,7 @@ write_to_path = "../site/generated_pages/"
 # edges associated with node 0:
 # DG[0]
 
-def yoga_journal(dictionary, image_width, small_font_size):
+def yoga_journal(dictionary: dict, image_width: str, small_font_size: str) -> str:
     str_to_write = ""
     if dictionary["yogajournal_picture"] != "":
         str_to_write+='\t<a href="' + dictionary["yogajournalurl"] + '">\n'
@@ -40,14 +41,14 @@ def yoga_journal(dictionary, image_width, small_font_size):
         str_to_write += '\t<font size="'+ small_font_size            + '">source:  <a href="'            + dictionary["yogajournalurl"]            + '">Yoga Journal</a></font><BR>\n'
     return str_to_write
 
-def dharma_mittra(dictionary, image_width, small_font_size):
+def dharma_mittra(dictionary: dict, image_width: str, small_font_size: str):
     str_to_write = ""
     if dictionary["Dharma Mittra picture URL"] != "":
         str_to_write += '\t<BR>\n\t<img src="'+ dictionary["Dharma Mittra picture URL"]+ '" width="'+ image_width+ '"><BR>\n'
         str_to_write += '\t<font size="'+ small_font_size            + '">source:  <a href="http://www.dharmayogacenter.com/resources/yoga-poses/view-all-yoga-poses-by-category/">dharmayogacenter.com</a></font><BR>\n'
     return str_to_write
 
-def local_images(pose_index_int: int, image_width):
+def local_images(pose_index_int: int, image_width: str, small_font_size: str):
     str_to_write = ""
     list_all_images = []
     list_all_images += glob.glob("../site/pose_pictures/*.jpg")
@@ -55,7 +56,24 @@ def local_images(pose_index_int: int, image_width):
     for img in list_all_images:
         filename = img.split("/")[-1]
         if filename.startswith(str(pose_index_int)+"__"):
-            str_to_write += "<img src=\""+img.replace("/site","")+"\" width=\""+image_width+"\"><BR/>\n"
+            str_to_write += "<P><img src=\""+img.replace("/site","")+"\" width=\""+image_width+"\"><BR/>\n"
+
+            is_img = False
+            if filename.endswith("jpg"):
+                source_filename = filename = img.split("/")[-1].replace("jpg","source")
+                is_img = True
+            elif filename.endswith("png"):
+                source_filename = filename = img.split("/")[-1].replace("png","source")
+                is_img = True
+            else:
+                print(filename + " is not an image")
+            if is_img:
+                print(source_filename)
+                if os.path.exists("../site/pose_pictures/"+source_filename):
+                    with open("../site/pose_pictures/"+source_filename, "r") as file_handle:
+                        file_content=file_handle.read()
+                    print(file_content)
+                    str_to_write += "<font size=\"" + small_font_size + "\"><a href=\""+file_content+"\">source</a></font></P>\n"
     return str_to_write
 
 for pose_index_int in range(len(DG)):
@@ -123,7 +141,7 @@ for pose_index_int in range(len(DG)):
                 f.write("\t<a href=\""+dic_for_this_node["wikipedia"]+"\">"+
                         dic_for_this_node["wikipedia"]+"</a><BR/>\n")
 
-        str_to_write = local_images(pose_index_int, image_width)
+        str_to_write = local_images(pose_index_int, image_width, small_font_size)
         f.write(str_to_write)
 
         str_to_write = yoga_journal(dic_for_this_node, image_width, small_font_size)
@@ -152,7 +170,7 @@ for pose_index_int in range(len(DG)):
                 + "</a></font><BR>\n"
             )
 
-            local_images(adjacent_pose_indx, image_width)
+            str_to_write = local_images(adjacent_pose_indx, image_width, small_font_size)
             f.write(str_to_write)
 
             str_to_write = yoga_journal(dic_for_adjacent_node, image_width, small_font_size)
